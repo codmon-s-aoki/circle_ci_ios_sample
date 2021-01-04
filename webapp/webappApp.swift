0e1b67cdcfa25c21cd14459c6758b84b92b59429
codmon-s-aoki <codmon-s-aoki@codmon.co.jp>
2021-01-05 06:23:31 +0900

    add xcode project

---
+++ webapp/webappApp.swift
@@ -0,0 +1,17 @@
+//
+//  webappApp.swift
+//  webapp
+//
+//  Created by satoshi aoki on 2021/01/05.
+//
+
+import SwiftUI
+
+@main
+struct webappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
